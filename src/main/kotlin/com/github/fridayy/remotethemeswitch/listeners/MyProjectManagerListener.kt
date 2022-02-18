012dce175da3724e5d89ff453bf02ddb9bb8bd1e
GitHub Action <action@github.com>
2022-02-18 20:52:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/fridayy/remotethemeswitch/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fridayy/remotethemeswitch/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fridayy.remotethemeswitch.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fridayy.remotethemeswitch.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/fridayy/remotethemeswitch/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fridayy/remotethemeswitch/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fridayy.remotethemeswitch.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fridayy.remotethemeswitch.MyBundle
 
 class MyProjectService(project: Project) {
 
